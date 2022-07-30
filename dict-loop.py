0201e93a3f7912cdbed917a0c321484fb116962b
YashdeepNeema <90140460+YashdeepNeema@users.noreply.github.com>
2022-07-30 17:31:45 +0530

    Add files via upload

---
+++ dict-loop.py
@@ -0,0 +1,10 @@
+# In this program I'm trying to learn how to loop through a dictionary:
+
+user_0 = {
+    'username': 'efermi',
+    'first_name': 'erico',
+    'last_name': 'fermi',
+}
+
+for key, value in user_0.items():
+    print(f'\n{key} : {value}');
